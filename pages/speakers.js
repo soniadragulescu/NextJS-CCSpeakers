9f1d93ee8801a111799361b1b8de148d6f2c3740
soniadragulescu <soniadr1999@yahoo.com>
2021-06-09 00:44:15 +0300

    server side rendering

--- pages/speakers.js
+++ pages/speakers.js
@@ -1,8 +1,30 @@
 import React from 'react'
 import App from '../src/App'
+import path from 'path'
+import fs from 'fs';
 
-function speakers () {
-  return <App pageName='Speakers'/>
+export async function getServerSideProps() {
+  const { promisify } = require('util');
+  const readFile = promisify(fs.readFile);
+  const jsonFile = path.resolve('./', 'db.json');
+  let initialSpeakerData;
+
+  try {
+    const readFileData = await readFile(jsonFile);
+    initialSpeakerData = JSON.parse(readFileData).speakers;
+
+  } catch (e) {
+    console.log('/api/speakers error: ', e)
+  }
+
+  return {
+    props: {initialSpeakerData}
+  }
+
+}
+
+function speakers({initialSpeakerData}) {
+  return <App pageName='Speakers' />
 }
 
 export default speakers
